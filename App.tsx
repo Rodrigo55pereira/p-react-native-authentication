71b10ef77eaf7a2829ffd733e455aa46f1773cbc
Rodrigo55pereira <rodrigo55pereira.com>
2025-03-29 12:48:19 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={s.container}>
+      <Text style={s.title}>Login</Text>
+      <TextInput placeholder="Email" style={s.input} />
+      <TextInput placeholder="Senha" style={s.input} secureTextEntry />
+      <Pressable
+        onPress={() => {
+          console.log("pressed");
+        }}
+        style={s.button}
+      >
+        <Text style={s.buttonText}>Entrar</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 20,
+    gap: 20,
   },
+  title: {
+    fontSize: 25,
+    fontWeight: "600",
+  },
+  input: {
+    borderWidth: 1,
+    padding: 15,
+    borderRadius: 5,
+    borderColor: "#CCC",
+  },
+  button: {
+    backgroundColor: "#4353FD",
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  }
 });
